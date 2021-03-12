b5c5921b2f54d463f1e46bc1b6aa253575aac889
Georgie Horth <georgiehorth@gmail.com>
2021-03-12 12:51:00 +0000

    Refactored test to reflect desired returned message, not an error. Passing.

--- dictionary.py
+++ dictionary.py
@@ -3,6 +3,9 @@ import json
 data = json.load(open("data.json"))
 
 def definition(word):
-    return data[word]
+    if word in data:
+        return data[word]
+    else:
+        return "That word doesn't exist. Please double check your spelling."
 
 print(definition("help"))
--- test_dictionary.py
+++ test_dictionary.py
@@ -8,5 +8,9 @@ def test_returns_definition():
     assert solution == ["The science that deals with concepts such as quantity, structure, space and change."]
 
 def test_fails_with_incorrect_word():
-    with pytest.raises(Exception):
-        definition("aaasdf")
+    # How to test for errors!!
+    # with pytest.raises(Exception):
+    #     definition("aaasdf")
+    solution = definition("aaasdf")
+
+    assert solution == "That word doesn't exist. Please double check your spelling."
