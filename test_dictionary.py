8d90223582a2a5334deb5b008ff8ca3f15803b62
Georgie Horth <georgiehorth@gmail.com>
2021-03-12 12:42:52 +0000

    Test for raising error with incorrect word passing

--- test_dictionary.py
+++ test_dictionary.py
@@ -1,6 +1,12 @@
+import pytest
+
 from dictionary import definition
 
 def test_returns_definition():
     solution = definition("mathematics")
 
     assert solution == ["The science that deals with concepts such as quantity, structure, space and change."]
+
+def test_fails_with_incorrect_word():
+    with pytest.raises(Exception):
+        solution = definition("aaasdf")
